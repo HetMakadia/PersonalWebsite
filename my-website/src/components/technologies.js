7c1c2be9cd873137ebf5bb8996d24b3afa40de22
HetMakadia <65148978+HetMakadia@users.noreply.github.com>
2023-03-09 00:39:20 -0800

    built up technologies.js for tech data

---
+++ my-website/src/components/technologies.js
@@ -0,0 +1,31 @@
+export const React = {
+    name: "React",
+    iconLink: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
+}
+
+export const Firebase = {
+    name: "Firebase",
+    iconLink: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+}
+
+export const TailwindCSS = {
+    name: "Tailwind CSS",
+    iconLink: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+}
+
+export const GoogleCloud = {
+    name: "Google Cloud",
+    iconLink: "https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg"
+}
+
+export const GoogleMapsAPI = {
+    name: "Google Maps API",
+    iconLink: "https://www.vectorlogo.zone/logos/google_maps/google_maps-icon.svg"
+}
+
+export const CalendlyAPI = {
+    name: "Calendly API",
+    iconLink: "https://github.com/wappalyzer/wappalyzer/blob/master/src/drivers/webextension/images/icons/Calendly.svg"
+}
+
+
