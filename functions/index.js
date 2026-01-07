a8a6de45f10e0367ec7f7c729f84a50c42d3a588
Het Makadia <hetmakadia.28@gmail.com>
2026-01-06 22:58:34 -0800

    get location bug fix

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,7 @@ const corsHandler = cors({
     "http://127.0.0.1:3000",
     "https://myportfolio-fbc40.web.app",
     "https://myportfolio-fbc40.firebaseapp.com",
+    "https://hetmakadia.com"
   ],
   methods: ["POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
